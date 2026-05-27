e08001f156b210fd089ec8b786cc105957b0ba7d
prinz40 <132628089+prinz40@users.noreply.github.com>
2026-05-27 04:37:04 +0100

    Add core payroll server layout

--- server.js
+++ server.js
@@ -53,3 +53,4 @@ app.post('/api/invoices', (req, res) => {
 
                                                                                                                                                         const PORT = 3000;
                                                                                                                                                         app.listen(PORT, () => console.log(`CreativePay Backend active out in the open on port ${PORT}`));
+                                                                                                                                                    
